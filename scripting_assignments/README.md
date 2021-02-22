543c39c51fb030f60891ca8aefc175c1d246ccbf
mithrim <mithrim@yandex.ru>
2021-02-22 17:09:06 +0300

    Rewrite README.md

--- scripting_assignments/README.md
+++ scripting_assignments/README.md
@@ -1,4 +1,13 @@
-This script display information about your connection to the internet. In accept thrree parameters for work:
-- type of connection, for e.g PID of connection, or application, wich established connection.
+#
+connection_info.sh
+
+##
+Please, enshure, that you have netstat and whois packages installed on your system before using this script.
+
+###
+This script display information about your connections to the internet. It accept three parameters for work:
+- script accept any type of information about connection fot filtering. You can enter part of application name
+  or connection state (case-insensetive search dosen't work, be carefully)
 - maximum number of displayed connections (max 50)
-- what kind of information do you know about connection, for e.g. Organization
+- what kind of information do you know about connection, for e.g. Organization. Also you can enter part of 
+  parameter. This parameter case-sensetive as first. 
--- scripting_assignments/connection_info.sh
+++ scripting_assignments/connection_info.sh
@@ -6,18 +6,18 @@ filter=$1
 number=$2
 info=$3
 
+#Define function for sorting unique addresses
 sorter(){
-	sort | uniq -c | sort
+	sort | uniq -c | sort | awk '{print $2}' | tail -n$(( $number + 1 ))
 }
 
 
 #Check count of accepted parameters
 if [ $# -ne 3 ]
 then
-	echo "This script needs three parameters for work! Please, enter following parameters!"
-	read -p "Enter PID of connection or application, which establishet connection, or connection status: " filter
-	read -p "Enter max number of unique connections you want to see at display: " number
-	read -p "What type of information about connection your looking for (Organization for e.g.): " info 
+	read -p "Enter first parameter: " filter
+	read -p "Enter second parameter: " number
+	read -p "Enter third parameter: " info 
 fi
 
 #Check parameters
@@ -31,8 +31,7 @@ then
 	exit 1
 elif [ "$number" -gt 50 ] 
 then 
-	echo "Your number is too big!"
-	exit 1
+	echo "Your number is too big!" 2>&1
 elif [ ! "$info" ]
 then
 	echo "Sorry, I don't know what are you waiting for("
@@ -42,17 +41,15 @@ fi
 
 #Create list of unique connections
 
-
-ip_string=$(sudo netstat -tunapl | awk '$0~$filter {print $5}'  | awk -F":" '{print $1}'| sorter | awk '{print $2}' | tail -n$number)
+ip_string=$(sudo netstat -tunapl | awk '$0~$filter {print $5}'  | awk -F":" '{print $1}'| sorter)
 
 
 for ip in $ip_string
 do
-#	echo "$ip"
 	if [ "$ip" = "Address" ]
 	then 
 		continue
 	else
-		whois "$ip" | grep -i "$info" | awk -F':' ' {print $2}' #Display information
+		whois "$ip" | awk -F':' '$0~$info {print $2}' #Display information 
 	fi
 done
